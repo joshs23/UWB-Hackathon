c4159195ad433b27d54624f7659a3bff97545adc
joshs <josh.sommer23@gmail.com>
2022-05-13 23:03:56 -0700

    Created a bare-bones structure for NewsStory object

--- NewsStory.cpp
+++ NewsStory.cpp
@@ -1,1 +1,20 @@
+#include "NewsStory.h"
 
+class NewsStory {
+
+    NewsStory::NewsStory() {}
+
+    bool NewsStory::isReal() {
+        return this->isReal;
+    }
+
+    std::string NewsStory::getArticleTitle() {
+        return this->articleTitle;
+    }
+
+    std::string NewsStory::getURL() {
+        return this->URL;
+    }
+
+
+};
--- NewsStory.h
+++ NewsStory.h
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <fstream>
 
+#pragma once
+
+class NewsStory{
+
+private:
+    bool isReal;
+    std::string URL;
+    std::string articleTitle;
+
+public:
+    NewsStory();
+    bool IsReal();
+    std::string getArticleTitle();
+    std::string getURL();
+
+};
