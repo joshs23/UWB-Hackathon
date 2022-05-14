935688ce03458661ec3915ea7080da87035051f4
skyla! <skylatran03@gmail.com>
2022-05-14 12:30:45 -0700

    describe plan in comments for the game files

--- GameCode.h
+++ GameCode.h
@@ -2,7 +2,21 @@
 #include "NewsStory.h"
 
 class GameCode {
-
+/* plan:
+ * from an array of news stories pick two true and one lie
+ * maybe two arrays, one full of true one full of false?
+ *
+ * put them in their own 3 elm array, save whatever index the fake one is in
+ * display the options to the user, have them decided what the fake is
+ *
+ * i think this is where the gui comes in? because we probably need buttons
+ * somehow
+ *
+ * keep track of points
+ * if they choose the correct answer add 10 points
+ * if they choose the wrong answer remove 10 points
+ * if they get x many wrong end game?
+ */
 
 
 
