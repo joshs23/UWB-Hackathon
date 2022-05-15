55a989f1bc8b4271f872e8b6b6122c499d7cdd75
skyla! <skylatran03@gmail.com>
2022-05-14 18:19:46 -0700

    added method headers to GameCode.h, started method definitions in GameCode.cpp

--- GameCode.cpp
+++ GameCode.cpp
@@ -1,4 +1,34 @@
 #include "GameCode.h"
 #include "NewsStory.h"
+#include <iostream>
 
+//constructor
+GameCode:: GameCode() {
+    stories = new NewsStory [3];
+};
+
+//chooses two random two, one random false to put in stories array
+void GameCode:: PickStories (int* trueStories, int* fakeStories) {
+
+}
+
+//return number of times guesses have been wrong in a row
+int GameCode:: GetNumWrongInRow(){
+
+}
+
+//returns the user's score
+int GameCode:: GetUserScore(){
+
+}
+
+//returns index of the fake story
+int GameCode::GetFakeStory(){
+
+}
+
+//destructor
+GameCode:: ~GameCode(){
+    delete [] stories;
+}
 
--- GameCode.h
+++ GameCode.h
@@ -2,6 +2,22 @@
 #include "NewsStory.h"
 
 class GameCode {
+//not sure what should be considered private
+private:
+    int wrongInARow;
+    int userScore;
+    int theFakeStory;
+    NewsStory* stories;
+
+public:
+    GameCode();
+    void PickStories (int* trueStories, int* fakeStories);
+    int GetNumWrongInRow();
+    int GetUserScore();
+    int GetFakeStory();
+    ~GameCode();
+
+
 /* plan:
  * from an array of news stories pick two true and one lie
  * maybe two arrays, one full of true one full of false?
@@ -15,10 +31,7 @@ class GameCode {
  * keep track of points
  * if they choose the correct answer add 10 points
  * if they choose the wrong answer remove 10 points
- * if they get x many wrong end game?
+ * if they get x many wrong in a row end game?
  */
-
-
-
 
 };
